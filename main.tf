3da67630f8277ecd3b11f26e64b1287eae98724a
Javid Salmanov <javid.salmanov@outlook.com>
2020-06-14 23:13:52 +0200

    Chnage main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
-provide "azurerm" {
+provider "azurerm" {
   version = "2.2.0"
   features {}
 }
 
 # Use alias if there are multiple subscription
-# provide "azurerm" {
+# provider "azurerm" {
 #   version = "2.2.0"
 #   features {}
 #   alias = "sub2"
 # }
+
+resource "azurerm_resource_group" "web_server_rg"{
+  name = "web_rg"
+  location = "westeurope"
+}
