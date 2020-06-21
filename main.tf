a6d6f9245261a7e32490c84fea8b47a0fd909a6a
Javid Salmanov <javid.salmanov@outlook.com>
2020-06-21 22:21:33 +0200

    Add data.tf and keep pass in Azure Key Vault

--- main.tf
+++ main.tf
@@ -126,11 +126,12 @@ resource "azurerm_virtual_machine_scale_set" "web-server" {
   os_profile {
     computer_name_prefix = local.web_server_name
     admin_username       = "adminuser"
-    admin_password       = "P@$$w0rd1234!"
+    admin_password       = data.azurerm_key_vault_secret.admin_password.value
   }
 
   os_profile_windows_config {
     provision_vm_agent = true
+    enable_automatic_upgrades = true
   }
 
   network_profile {
