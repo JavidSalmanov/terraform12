bf72edd0109eeb1c250c5c7c3fd6f2298c1c0f3b
Javid Salmanov <javid.salmanov@outlook.com>
2020-06-22 21:52:30 +0200

    Add output

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "bastion_host_subnet_westeurope"{
+    value = module.location_westeurope.bastion_host_subnet
+}
+ 
+output "bastion_host_subnet_northeurope"{
+    value = module.location_northeurope.bastion_host_subnet
+}
